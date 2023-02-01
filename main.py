f749593bb63e1b63ae4b6b2930a27366b299ba6e
seokcode <ssd3784@gmail.com>
2023-02-01 23:45:30 +0900

    main.py 수정

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
-    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
